37936fc6508b905f87588d65b3d6b97821d4b99c
Dhifli Med <mohamed.dhifli@b4creation.org>
2023-02-05 23:06:24 +0100

    firstCommit.

---
+++ src/pages/csr.js
@@ -0,0 +1,27 @@
+// client side rendered
+import { useEffect, useState } from "react";
+
+export default function ClientSideRendered() {
+    const [state, setState] = useState([]);
+
+  async function getData() {
+    const res = await fetch ('https://63e014158b24964ae0f92fa7.mockapi.io/names/names');
+    const data = await res.json();
+    setState(data);
+  }
+  
+  useEffect(() => {
+   getData();
+  },[]);
+
+  console.log("state = "+state);
+
+  return (
+    <>
+       {state.map((e) => (
+         <h2 key={e.id}>{e.name}</h2>
+        ))}
+    </>)
+
+
+}
---
+++ src/pages/ssr.js
@@ -0,0 +1,23 @@
+// server side rendering
+import { useEffect, useState } from "react";
+
+export default function ServerSideRenderd({ state }) {
+    return (
+        <>
+            {state.map((e) => (
+                <h2 key={e.id}>{e.name}</h2>
+            ))}
+        </>)
+}
+
+export async function getServerSideProps() {
+    const res = await fetch('https://63e014158b24964ae0f92fa7.mockapi.io/names/names');
+    const state = await res.json();
+
+    return {
+        props: {
+            state, //will be passed to the page component as props
+        },
+    };
+}
+
